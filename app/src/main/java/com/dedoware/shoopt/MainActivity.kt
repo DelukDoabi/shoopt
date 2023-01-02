b6a5fa5d7fccd37f0292cf1091faafe891adde89
Deluk Doabi <deluk.doabi@gmail.com>
2023-01-03 00:03:08 +0100

    construction: setup all Id

--- app/src/main/java/com/dedoware/shoopt/MainActivity.kt
+++ app/src/main/java/com/dedoware/shoopt/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.dedoware.shoopt
 
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
+
+    // TODO NBE if not working put findViewById methods after setContentView
+    private val addOrUpdateProductImageButton: ImageButton =
+        findViewById(R.id.add_or_update_product_IB)
+    private val trackShoppingImageButton: ImageButton =
+        findViewById(R.id.track_shopping_IB)
+    private val analyseImageButton: ImageButton =
+        findViewById(R.id.analyse_IB)
+
+    private val addOrUpdateProductTextView: TextView =
+        findViewById(R.id.add_or_update_product_TV)
+    private val trackShoppingTextView: TextView =
+        findViewById(R.id.track_shopping_TV)
+    private val analyseTextView: TextView =
+        findViewById(R.id.analyse_TV)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addOrUpdateProductImageButton.setOnClickListener {}
     }
+
 }
