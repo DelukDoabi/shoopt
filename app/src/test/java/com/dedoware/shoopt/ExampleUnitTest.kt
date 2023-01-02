d7e499efc649f5ccb53370619ea66a2aff9704ae
Deluk Doabi <deluk.doabi@gmail.com>
2023-01-02 15:24:51 +0100

    :art: format code

--- app/src/test/java/com/dedoware/shoopt/ExampleUnitTest.kt
+++ app/src/test/java/com/dedoware/shoopt/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.dedoware.shoopt
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
