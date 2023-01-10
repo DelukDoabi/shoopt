e24516877af6536f1408bbcb8c5c13372fb370fa
DelukDoabi <deluk.doabi@gmail.com>
2023-01-10 17:53:37 +0100

    :bug: Fix Launch (#1)
    * :bug: fix launch (remove Shoopt theme)
    
    * :construction: improve theme customization
    
    * :bug: put back default theme colors
    
    * :bulb: add comment to remember default VS custom part colors

--- app/src/main/java/com/dedoware/shoopt/MainActivity.kt
+++ app/src/main/java/com/dedoware/shoopt/MainActivity.kt
@@ -9,25 +9,37 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     // TODO NBE if not working put findViewById methods after setContentView
-    private val addOrUpdateProductImageButton: ImageButton =
-        findViewById(R.id.add_or_update_product_IB)
-    private val trackShoppingImageButton: ImageButton =
-        findViewById(R.id.track_shopping_IB)
-    private val analyseImageButton: ImageButton =
-        findViewById(R.id.analyse_IB)
-
-    private val addOrUpdateProductTextView: TextView =
-        findViewById(R.id.add_or_update_product_TV)
-    private val trackShoppingTextView: TextView =
-        findViewById(R.id.track_shopping_TV)
-    private val analyseTextView: TextView =
-        findViewById(R.id.analyse_TV)
+    private lateinit var addOrUpdateProductImageButton: ImageButton
+    private lateinit var trackShoppingImageButton: ImageButton
+    private lateinit var analyseImageButton: ImageButton
+    private lateinit var addOrUpdateProductTextView: TextView
+    private lateinit var trackShoppingTextView: TextView
+    private lateinit var analyseTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+// TODO NBE if not working put findViewById methods after setContentView
+        setMainVariables()
+
         addOrUpdateProductImageButton.setOnClickListener {}
+    }
+
+    private fun setMainVariables() {
+        addOrUpdateProductImageButton =
+            findViewById(R.id.add_or_update_product_IB)
+        trackShoppingImageButton =
+            findViewById(R.id.track_shopping_IB)
+        analyseImageButton =
+            findViewById(R.id.analyse_IB)
+
+        addOrUpdateProductTextView =
+            findViewById(R.id.add_or_update_product_TV)
+        trackShoppingTextView =
+            findViewById(R.id.track_shopping_TV)
+        analyseTextView =
+            findViewById(R.id.analyse_TV)
     }
 
 }
