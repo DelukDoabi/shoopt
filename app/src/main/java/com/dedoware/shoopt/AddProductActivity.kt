7bff0e003afdb886b4ddd086b5150ebaa52103d8
DelukDoabiV <deluk.doabi@gmail.com>
2023-01-11 15:28:50 +0100

    :construction: add back button event

--- app/src/main/java/com/dedoware/shoopt/AddProductActivity.kt
+++ app/src/main/java/com/dedoware/shoopt/AddProductActivity.kt
@@ -1,11 +1,27 @@
 package com.dedoware.shoopt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 
 class AddProductActivity : AppCompatActivity() {
+    private lateinit var backImageButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
+
+        // TODO NBE if not working put findViewById methods after setContentView
+        setMainVariables()
+
+        backImageButton.setOnClickListener {
+            finish()
+        }
+    }
+
+
+    private fun setMainVariables() {
+        backImageButton =
+            findViewById(R.id.back_IB)
     }
 }
