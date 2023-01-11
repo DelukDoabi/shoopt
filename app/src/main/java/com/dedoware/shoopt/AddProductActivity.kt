0a762bb4eff2b3acc0e10d83cb1956c11e5e1ead
DelukDoabiV <deluk.doabi@gmail.com>
2023-01-11 14:35:00 +0100

    :construction: add manually add product view

---
+++ app/src/main/java/com/dedoware/shoopt/AddProductActivity.kt
@@ -0,0 +1,11 @@
+package com.dedoware.shoopt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_product)
+    }
+}
--- app/src/main/java/com/dedoware/shoopt/MainActivity.kt
+++ app/src/main/java/com/dedoware/shoopt/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.dedoware.shoopt
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,10 +23,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-// TODO NBE if not working put findViewById methods after setContentView
+        // TODO NBE if not working put findViewById methods after setContentView
         setMainVariables()
 
-        addOrUpdateProductImageButton.setOnClickListener {}
+        addOrUpdateProductImageButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Choose an option")
+            builder.setMessage("Scan barcode or add product manually")
+
+            builder.setPositiveButton("Scan barcode") { _, _ ->
+                // Launch the barcode scanner
+                IntentIntegrator(this).initiateScan()
+            }
+
+            builder.setNegativeButton("Add product manually") { _, _ ->
+                // Launch the add product manually activity
+                val addProductIntent = Intent(this, AddProductActivity::class.java)
+                startActivity(addProductIntent)
+            }
+
+            val dialog = builder.create()
+            dialog.show()
+        }
     }
 
     private fun setMainVariables() {
@@ -35,7 +56,7 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.analyse_IB)
 
         addOrUpdateProductTextView =
-            findViewById(R.id.add_or_update_product_TV)
+            findViewById(R.id.save_product_TV)
         trackShoppingTextView =
             findViewById(R.id.track_shopping_TV)
         analyseTextView =
