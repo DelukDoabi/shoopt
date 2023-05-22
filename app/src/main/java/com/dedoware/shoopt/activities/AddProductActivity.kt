8d73c1b8a58cf4b097176df34c54e7e2db5f45fe
DelukDoabi <deluk.doabi@gmail.com>
2023-05-23 00:30:36 +0200

    ✨ : Add update product feature (#19)
    ✨ Add the ability to update an existing product (by modifying its data (details))

--- app/src/main/java/com/dedoware/shoopt/activities/AddProductActivity.kt
+++ app/src/main/java/com/dedoware/shoopt/activities/AddProductActivity.kt
@@ -17,12 +17,14 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import androidx.core.graphics.drawable.toBitmap
+import com.bumptech.glide.Glide
 import com.dedoware.shoopt.R
 import com.dedoware.shoopt.model.Product
 import com.dedoware.shoopt.model.Shop
 import com.dedoware.shoopt.utils.ShooptUtils
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
@@ -31,6 +33,7 @@ import java.io.File
 
 
 class AddProductActivity : AppCompatActivity() {
+    private lateinit var retrievedProductId: String
     private lateinit var backImageButton: ImageButton
     private lateinit var productPictureImageButton: ImageButton
 
@@ -62,6 +65,8 @@ class AddProductActivity : AppCompatActivity() {
 
         setMainVariables()
 
+        retrieveProductDataFromIntent()
+
         retrieveMainData(savedInstanceState)
 
         ShooptUtils.doAfterInitFirebase(baseContext) { setShopsData() }
@@ -82,7 +87,11 @@ class AddProductActivity : AppCompatActivity() {
         }
 
         saveProductImageButton.setOnClickListener {
-            saveAllProductDataInFirebase()
+            if (intent.hasExtra("productId"))
+                intent.getStringExtra("pictureUrl")
+                    ?.let { it -> saveProductInFirebaseDatabase(it) }
+            else
+                saveAllProductDataInFirebase()
         }
     }
 
@@ -259,17 +268,85 @@ class AddProductActivity : AppCompatActivity() {
         val shop = productShopAutoCompleteTextView.text.toString()
 
         if (name.isNotEmpty() && !price.isNaN() && !unitPrice.isNaN() && shop.isNotEmpty()) {
-            addProductToRTDB(
-                barcode,
-                timestamp,
-                name,
-                shop,
-                price,
-                unitPrice,
-                productPictureUrl
-            )
+            val productsRef = ShooptUtils.getFirebaseDatabaseReference().child("products")
+
+            if (intent.hasExtra("productId")) {
+                // Product already exists, update its data
+                intent.getStringExtra("productId")?.let {
+                    updateProductInRTDB(
+                        it,
+                        barcode,
+                        timestamp,
+                        name,
+                        price,
+                        unitPrice,
+                        shop,
+                        productPictureUrl,
+                        productsRef
+                    )
+                }
+            } else {
+                // Product does not exist, create a new one
+                addProductToRTDB(
+                    barcode,
+                    timestamp,
+                    name,
+                    shop,
+                    price,
+                    unitPrice,
+                    productPictureUrl
+                )
+            }
         } else {
             displayFailedStorage()
+        }
+    }
+
+    private fun updateProductInRTDB(
+        productId: String,
+        barcode: Long,
+        timestamp: Long,
+        name: String,
+        price: Double,
+        unitPrice: Double,
+        shop: String,
+        productPictureUrl: String,
+        productsRef: DatabaseReference
+    ) {
+            if (productId != null) {
+                val updatedProduct = Product(
+                    productId,
+                    barcode,
+                    timestamp,
+                    name,
+                    price,
+                    unitPrice,
+                    shop,
+                    productPictureUrl
+                )
+
+                // Update the product in the database
+                productsRef.child(productId).setValue(updatedProduct)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("SHOOPT_TAG", "Product updated!")
+                            Toast.makeText(
+                                this@AddProductActivity,
+                                "Product updated!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            Log.d("SHOOPT_TAG", "Error updating product: ${task.exception}")
+                        }
+                    }
+                    .addOnFailureListener { e ->
+                        Log.d("SHOOPT_TAG", e.localizedMessage ?: "Failed to update product")
+                        Toast.makeText(
+                            this@AddProductActivity,
+                            e.localizedMessage,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
         }
     }
 
@@ -383,4 +460,30 @@ class AddProductActivity : AppCompatActivity() {
             productShopAutoCompleteTextView.setText(savedInstanceState.getString("productShop"))
         }
     }
+
+    private fun retrieveProductDataFromIntent() {
+        val productId = intent.getStringExtra("productId")
+        val barcode = intent.getLongExtra("barcode", 0L)
+        val name = intent.getStringExtra("name")
+        val shop = intent.getStringExtra("shop")
+        val price = intent.getDoubleExtra("price", 0.0)
+        val unitPrice = intent.getDoubleExtra("unitPrice", 0.0)
+        val pictureUrl = intent.getStringExtra("pictureUrl")
+
+        if (productId != null && barcode != 0L && name != null && shop != null && price != 0.0 && unitPrice != 0.0) {
+            retrievedProductId = productId
+            productBarcodeEditText.setText(barcode.toString())
+            productNameEditText.setText(name)
+            productShopAutoCompleteTextView.setText(shop)
+            productPriceEditText.setText(price.toString())
+            productUnitPriceEditText.setText(unitPrice.toString())
+
+            // Load the product picture from the pictureUrl using Glide with resizing options
+            Glide.with(this)
+                .load(pictureUrl)
+                .override(300, 300)
+                .into(productPictureImageButton)
+        }
+    }
+
 }
